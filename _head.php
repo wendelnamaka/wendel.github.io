c6792910498a17ba9f26e0eea3410dae43e1bfbf
wendelnamaka <wendeldacostaneves@gmail.com>
2020-02-20 11:21:04 -0300

    Portifolio
    https://wendelnamaka.github.io/wendenamaka/

---
+++ _head.php
@@ -0,0 +1,18 @@
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Stacked - Free CSS Template</title>
+<!--
+Stacked Template
+http://www.templatemo.com/tm-505-stacked
+-->
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+        <link rel="stylesheet" href="css/templatemo-style.css">
+
+        <script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+    </head>
---
+++ _menu.php
@@ -0,0 +1,20 @@
+<nav class="nav">
+    <div class="burger">
+        <div class="burger__patty"></div>
+    </div>
+
+    <ul class="nav__list">
+        <li class="nav__item">
+            <a href="#1" class="nav__link c-blue"><img src="img/home-icon.png" alt=""></a>
+        </li>
+        <li class="nav__item">
+            <a href="#2" class="nav__link c-yellow scrolly"><img src="img/about-icon.png" alt=""></a>
+        </li>
+        <li class="nav__item">
+            <a href="#3" class="nav__link c-red"><img src="img/projects-icon.png" alt=""></a>
+        </li>
+        <li class="nav__item">
+            <a href="#4" class="nav__link c-green"><img src="img/contact-icon.png" alt=""></a>
+        </li>
+    </ul>
+</nav>
